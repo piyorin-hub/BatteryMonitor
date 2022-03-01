adaf4c04a241130e720bcb28b0515354763c6842
piyorin85 <noripmark@gmail.com>
2022-03-02 00:49:09 +0900

    first commit

--- BatteryMonitor/ViewController.swift
+++ BatteryMonitor/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var batteryLevel: UILabel!
+    @IBOutlet weak var batteryImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
